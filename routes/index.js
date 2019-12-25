33eaed9cd3a8e94c3a4f327e85eab7fbc66d178d
Sven Christ <schrist2@smail.uni-koeln.de>
2019-12-25 14:24:46 +0100

    Added MySQL connection.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        port: process.env.DB_PORT
+    });
+
+    connection.connect(function(err) {
+        if (err) {
+            res.status(400);
+            res.send('MySQL connection failed: ' + err);
+            return;
+        }
+
+        connection.end();
+
+        res.render('index', { title: 'Express' });
+    });
 });
 
 module.exports = router;
